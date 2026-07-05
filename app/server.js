1f291a036fbf7f5ada6af3fd0bc1e1dc092b16d4
imene.saadani <saadaniimene@gmail.com>
2026-07-05 13:49:57 +0100

    init ci pipeline

---
+++ app/server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express();
+
+app.get("/hello", (req, res) => {
+  res.json({
+	  message: "Hello DevOps from Node.js: imene",
+    version: "1.0.0"
+  });
+});
+
+app.get("/health", (req, res) => {
+  res.json({
+    status: "UP"
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`API running on port ${PORT}`);
+});
